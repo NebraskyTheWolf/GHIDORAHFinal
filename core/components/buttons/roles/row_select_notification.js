59fd1b1e441fc0c5e84ef862890e21104fd12f36
NebraskyTheWolf <matazox302@gmail.com>
2022-12-28 16:03:24 +0000

    Update

--- core/components/buttons/roles/row_select_notification.js
+++ core/components/buttons/roles/row_select_notification.js
@@ -2,11 +2,8 @@ module.exports = {
     data: {
         name: "row_select_notification"
     },
-    getRoleByID(guild, roleId) {
-        return client.guilds.cache.get(guild.id).roles.cache.get(roleId);
-    },
     async execute(interaction, interactionUser, guild) {
-        let role = this.getRoleByID(guild, interaction.values[0]);
+        let role = client.guilds.cache.get(guild.id).roles.cache.get(interaction.values[0]);
         if (interactionUser.roles.get(role)) {
             interactionUser.roles.remove(role);
             interaction.reply({ content: 'Role removed.', ephemeral: true, });
